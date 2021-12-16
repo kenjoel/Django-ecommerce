e47b9d2240dc66b94fbfc2e2f9f593655a72fb21
kenjoel <jrjoemuigai@gmail.com>
2021-12-16 22:53:03 +0300

    UI for add to cart

--- ecom/core/views.py
+++ ecom/core/views.py
@@ -50,14 +50,18 @@ def add_to_cart(request, slug):
             order_item.quantity += 1
             order_item.save()
             messages.info(request, f"You purchased {order_item.quantity} more of this item")
+            return redirect('core:order_summary')
         else:
             messages.info(request, "This item was added to your cart")
             order.items.add(order_item)
+            return redirect('core:order_summary')
     else:
         order_date = timezone.now()
         order = Order.objects.create(user=request.user, ordered_date=order_date)
         order.items.add(order_item)
-    return redirect('core:product', slug=slug)
+        messages.info(request, "New item was added to your cart")
+        return redirect('core:order_summary')
+
 
 
 @login_required
@@ -80,3 +84,22 @@ def remove_from_cart(request, slug):
         else:
             messages.info(request, "This item was Not in your cart")
             return redirect('core:product', slug=slug)
+
+@login_required
+def remover_single_item(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+            if order_item.quantity > 1:
+                order_item.quantity -= 1
+                order_item.save()
+                messages.info(request, "One of these item was Removed from your cart")
+                return redirect('core:order_summary')
