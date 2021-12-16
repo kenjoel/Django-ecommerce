3192e31235a2bfa1b051a7e4124efce0af32ad71
kenjoel <jrjoemuigai@gmail.com>
2021-12-16 22:19:05 +0300

    cart summary added

--- ecom/core/urls.py
+++ ecom/core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import HomeView, ItemDetailView, add_to_cart, remove_from_cart
+from .views import HomeView, ItemDetailView, add_to_cart, remove_from_cart, OrderSummaryView
 
 app_name = 'core'
 urlpatterns = [
@@ -8,6 +8,7 @@ urlpatterns = [
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     path('add_to_cart/<slug>/', add_to_cart, name='add_to_cart'),
     path('remove_from_cart/<slug>/', remove_from_cart, name='remove_from_cart'),
+    path('order_summary/', OrderSummaryView.as_view(), name='order_summary'),
 
     # path('<int:pk>/', item_detail, name='item_detail'),
 ]
--- ecom/core/views.py
+++ ecom/core/views.py
@@ -1,6 +1,10 @@
 from django.contrib import messages
-from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
+from django.views import View
 from django.views.generic import ListView, DetailView
 
 from .models import Item, OrderItem, Order
@@ -12,11 +16,24 @@ class HomeView(ListView):
     template_name = 'home-page.html'
 
 
+class OrderSummaryView(LoginRequiredMixin, View):
+    def get(self, *args, **kwargs):
+        try:
+            order = Order.objects.get(user=self.request.user, ordered=False)
+            context = {
+                'object': order
+            }
+            return render(self.request, 'order_summary.html', context)
+        except ObjectDoesNotExist:
+            messages.error(self.request, "You do not have an active order")
+            return redirect("/")
+
+
 class ItemDetailView(DetailView):
     model = Item
     template_name = 'product.html'
 
-
+@login_required
 def add_to_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
     order_item, created = OrderItem.objects.get_or_create(
@@ -43,6 +60,7 @@ def add_to_cart(request, slug):
     return redirect('core:product', slug=slug)
 
 
+@login_required
 def remove_from_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
     order_qs = Order.objects.filter(user=request.user, ordered=False)
