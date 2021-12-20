f28496fd3b618f5104c9919a944e08ea54514fd3
kenjoel <jrjoemuigai@gmail.com>
2021-12-20 09:02:36 +0300

    retry

--- ecom/settings.py
+++ ecom/settings.py
@@ -30,7 +30,7 @@ STRIPE_SECRET_KEY = env("STRIPE_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["django-deploymentken", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
