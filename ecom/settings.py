c3cf453bcdbb0a79c0b5bda2667e72232d5b75bc
kenjoel <jrjoemuigai@gmail.com>
2021-12-20 09:55:23 +0300

    static files

--- ecom/settings.py
+++ ecom/settings.py
@@ -140,7 +140,7 @@ AUTHENTICATION_BACKENDS = [
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
